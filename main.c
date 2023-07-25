93c7eb8ecbc7beab755c280ea2e0e1c57f54527f
Boitumelo <bcrachoshi@gmail.com>
2023-07-25 20:51:05 +0200

    final backup

--- main.c
+++ main.c
@@ -1,27 +1,53 @@
 #include "shell.h"
+
 /**
  * main - Entry point for the Simple Shell program.
- * Return:EXIT_SUCCESS (0) to indicate successful termination.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of pointers to command-line arguments.
+ * Return: EXIT_SUCCESS (0) to indicate successful termination.
  */
-
-int main(void)
+int main(int argc, char *argv[])
 {
-	char *command;
-
-	while (1)
+	if (argc > 1)
 	{
-		display_prompt();
-		command = custom_getline();
-
-		if (!command)
+		FILE *file = fopen(argv[1], "r");
+		if (!file)
 		{
-			printf("\n");
-			break;
+			perror("Error opening file");
+			return (1);
 		}
 
-		execute_command(command);
-		free(command);
+		char line[LINE_BUFFER_SIZE];
+		while (fgets(line, LINE_BUFFER_SIZE, file) != NULL)
+		{
+			char *pos;
+			if ((pos = strchr(line, '\n')) != NULL)
+				*pos = '\0';
+
+			execute_command(line);
+		}
+
+		fclose(file);
+	}
+	else
+	{
+		char *command;
+
+		while (1)
+		{
+			display_prompt();
+			command = custom_getline();
+
+			if (!command)
+			{
+				printf("\n");
+				break;
+			}
+
+			execute_command(command);
+			free(command);
+		}
 	}
 
-return (EXIT_SUCCESS);
+	return (0);
 }
